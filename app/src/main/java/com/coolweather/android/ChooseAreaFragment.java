a34f695be593cebe1d53f547ab250476332fce3d
MyTrumpet <MyTrumpet@qq.com>
2020-06-05 04:26:13 +0000

    weather information

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -40,6 +40,7 @@ public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
     public static final int LEVEL_COUNTY = 2;
+    
     /**
      * 准备一些控件
      */
